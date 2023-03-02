f1dde7fc9fdc3e17bc3cf790fa2ce28f7ed12a33
Aleksandra Grzeczyńska <puszka004@gmail.com>
2023-03-02 19:04:45 +0100

    Initial commit

---
+++ src/test/java/com/worldwidewindsurfersweatherservice/WorldwideWindsurfersWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldwidewindsurfersweatherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldwideWindsurfersWeatherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
